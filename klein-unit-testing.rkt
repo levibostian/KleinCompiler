a316ac6e9f752c61f969c76705d7ba3d50dc6fa0
Levi Bostian <bostianl@uni.edu>
2013-09-23 00:01:31 -0500

    Add basic token-factory to return string value for each string given.

---
+++ klein-unit-testing.rkt
@@ -0,0 +1,19 @@
+#lang racket
+; Klein compiler - UNIT TESTING
+; cs4550 - Fall 2013
+; 
+; Team RackAttack
+
+(require rackunit
+         "klein.rkt")
+
+;we do not have finished compiler yet, so cannot test yet
+;(check-eq? (klein "path-to-source.kln") )
+
+(check-equal? (token-factory "boolean") "<type> boolean")
+(check-equal? (token-factory "integer") "<type> integer")
+(check-equal? (token-factory "(") "<punctuation> (")
+(check-equal? (token-factory ")") "<punctuation> )")
+(check-equal? (token-factory ":") "<punctuation> :")
+(check-equal? (token-factory ",") "<punctuation> ,")
+(check-equal? (token-factory "rackAttack") "<identifier> rackAttack")
---
+++ klein.rkt
@@ -0,0 +1,44 @@
+#lang racket
+; Klein compiler
+; cs4550 - Fall 2013
+; 
+; Team RackAttack
+
+(provide klein
+         line-reader
+         char-reader
+         token-factory)
+
+(define klein
+  (lambda (source-code-path)
+    (line-reader "" (open-input-file source-code-path)) ))
+
+(define line-reader
+  (lambda (current-code-line input-port)
+    ; send line of code to function that reads character by character
+    (char-reader 
+   ; (line-reader (line-read input-port) input-port)
+    )))
+
+;(define char-reader
+;  (lambda (char accum-string)
+;    (if (or (eq? char " ")
+;            (eq? char "(")
+;            (eq? char ")")
+;            (eq? char ":")
+;            (eq? char ","))
+;        ;run accum through function for token creation
+;        (token-factory accum-string)
+;        "else")
+;        ))
+
+(define token-factory
+  (lambda (accum)
+    (cond ((or (eq? accum "integer")
+               (eq? accum "boolean")) (string-append "<type> " accum))
+          ((or (eq? accum "(")
+               (eq? accum ")")
+               (eq? accum ":")
+               (eq? accum ",")) (string-append "<punctuation> " accum))
+          (else (string-append "<identifier> " accum))) ))
+           
