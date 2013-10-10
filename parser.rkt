ab56647ec8e08bca3c33f6f86169e3f420ac891e
Levi Bostian <bostianl@uni.edu>
2013-10-10 12:12:04 -0500

    Remove file read properties in parser as that is not needed. Scanner reads file for you.

---
+++ parser.rkt
@@ -0,0 +1,22 @@
+#lang racket
+; Klein compiler
+; cs4550 - Fall 2013
+; 
+; Team RackAttack
+
+(require "scanner.rkt")
+
+(provide (all-defined-out))
+
+(define parser
+  (lambda (source-code-path)
+      (token-reader (scanner source-code-path)) ))
+
+(define token-reader
+  (lambda (token-list)
+    (token-reader-helper '() '('$) token-list) ))
+
+(define token-reader-helper
+  (lambda (parser-accum stack token-list)
+    
+    ))
