ffba9fae1fc8aea227e41d1fbf44036b69e56500
Levi Bostian <bostianl@uni.edu>
2013-11-21 11:56:47 -0600

    kleinc bash file sends output *.tm file to generate function.

--- bash-files/run-code-generator.rkt
+++ bash-files/run-code-generator.rkt
@@ -6,5 +6,7 @@
 
 (define file-name
   (vector-ref (current-command-line-arguments) 0))
+(define tm-file-name
+  (vector-ref (current-command-line-arguments) 1))
 
-(generate (semantic-actions (parser file-name)))
+(generate (semantic-actions (parser file-name)) tm-file-name)
